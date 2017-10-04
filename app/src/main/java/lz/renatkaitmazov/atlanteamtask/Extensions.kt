6d4ddd0ddcec86b2aa43ea3c79eb8aa2329dc335
Renat Kaitmazov <renatkaitmazov@gmail.com>
2017-10-04 18:50:15 +0300

    Create layout of the main activity

---
+++ app/src/main/java/lz/renatkaitmazov/atlanteamtask/Extensions.kt
@@ -0,0 +1,34 @@
+package lz.renatkaitmazov.atlanteamtask
+
+import android.annotation.SuppressLint
+import android.support.annotation.IdRes
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+
+/**
+ *
+ * @author Renat Kaitmazov
+ */
+
+/*------------------------------------------------------------------------*/
+// Activity Extensions
+/*------------------------------------------------------------------------*/
+
+@SuppressLint("CommitTransaction")
+fun AppCompatActivity.addFragment(fragment: Fragment, @IdRes containerId: Int) {
+    val fragmentManager = supportFragmentManager
+    val currentFragment = fragmentManager.findFragmentById(containerId)
+    if (currentFragment == null) {
+        // The container was empty.
+        fragmentManager.beginTransaction()
+                .add(containerId, fragment)
+                .commitNow()
+    }
+}
+
+@SuppressLint("CommitTransaction")
+fun AppCompatActivity.replaceFragment(fragment: Fragment, @IdRes containerId: Int) {
+    supportFragmentManager.beginTransaction()
+            .replace(containerId, fragment)
+            .commitNow()
+}
--- app/src/main/java/lz/renatkaitmazov/atlanteamtask/main/MainActivity.kt
+++ app/src/main/java/lz/renatkaitmazov/atlanteamtask/main/MainActivity.kt
@@ -2,9 +2,9 @@ package lz.renatkaitmazov.atlanteamtask.main
 
 import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import lz.renatkaitmazov.atlanteamtask.R
-import lz.renatkaitmazov.atlanteamtask.app.AtlanApp
 import lz.renatkaitmazov.atlanteamtask.databinding.ActivityMainBinding
 
 /**
@@ -27,5 +27,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        initBottomNavView(binding.bottomView)
+    }
+
+    private fun initBottomNavView(navView: BottomNavigationView) {
+        navView.selectedItemId = R.id.menu_cards
+        navView.setOnNavigationItemSelectedListener listener@ { menuItem ->
+            when (menuItem.itemId) {
+                R.id.menu_cards -> println("Clicked " + menuItem.title)
+                R.id.menu_my_contacts -> println("Clicked " + menuItem.title)
+            }
+            return@listener true
+        }
     }
 }
